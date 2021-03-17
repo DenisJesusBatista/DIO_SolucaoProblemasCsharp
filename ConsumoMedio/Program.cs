2c1e21a8aa64e3b0318291864bc5538a4650be44
Denis Batista <denis.si2011@gmail.com>
2021-03-17 19:54:05 -0300

    Correção de programas em C#

---
+++ ConsumoMedio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumoMedio
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia;
+            double combustivelGasto, consumoMedio;
+
+            Console.WriteLine("Informa a distância peccorrida.");
+            distancia = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informa o combustivél gasto.");
+            combustivelGasto = Convert.ToDouble(Console.ReadLine());
+
+            consumoMedio = distancia / combustivelGasto;//Formula para que o codigo funcione corretamente
+            Console.WriteLine(" km/l {0:0.000}", consumoMedio);
+            Console.ReadLine();
+        }
+    }
+}
